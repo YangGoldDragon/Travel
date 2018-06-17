a9a063338c7a35f0e6b43adc08134efed458f406
yangjinlong <835383258@qq.com>
2018-06-18 03:45:50 +0800

    swiper.vue

--- src/main.js
+++ src/main.js
@@ -4,12 +4,18 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 import fastClick from 'fastclick';
+import VueAwesomeSwiper from 'vue-awesome-swiper';
+
 import './assets/styles/reset.css';
 import './assets/styles/border.css';
 import './assets/styles/iconfont.css';
+import 'swiper/dist/css/swiper.css';
 
+Vue.use(VueAwesomeSwiper);
 Vue.config.productionTip = false;
 fastClick.attach(document.body);
+
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
---
+++ src/pages/home/components/VueAwesomeSwiper.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item in imgList" :key="item.id">
+        <img :src="item.imgUrl" alt="" class="swiper-img">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'VueAwesomeSwiper',
+    data() {
+      return {
+        swiperOption: {
+          pagination: '.swiper-pagination',
+          loop: true
+        },
+        imgList: [
+          {
+            id: '001',
+            imgUrl: '//img1.qunarzz.com/qs/1806/8f/e1141f1b52114402.jpg'
+          },
+          {
+            id: '002',
+            imgUrl: '//img1.qunarzz.com/qs/1805/79/895429b453f08002.jpg'
+          },
+          {
+            id: '003',
+            imgUrl: '//img1.qunarzz.com/qs/1806/98/57120f8844f23e02.jpg'
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style  lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background white !important
+  .wrapper
+    overflow hidden
+    width 100%
+    height 0
+    padding-bottom 25.6%
+    background #eee
+    .swiper-img
+      width 100%
+</style>
