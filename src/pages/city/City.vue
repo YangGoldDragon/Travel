e302c38f1b354684d78178f14e64f1b51663ea0c
yangjinlong <835383258@qq.com>
2018-06-30 17:32:08 +0800

    finish CityAlphabet

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,17 +2,20 @@
   <div>
     <city-header></city-header>
     <city-list></city-list>
+    <city-alphabet></city-alphabet>
   </div>
 </template>
 
 <script>
   import CityHeader from './components/CityHeader.vue';
   import CityList from './components/CityList.vue';
+  import CityAlphabet from './components/CityAlphabet.vue';
   export default {
     name: 'City',
     components: {
       CityHeader,
-      CityList
+      CityList,
+      CityAlphabet
     }
   }
 </script>
---
+++ src/pages/city/components/CityAlphabet.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="wrapper">
+    <ul class="alphabet">
+      <li>A</li>
+      <li>A</li>
+      <li>A</li>
+      <li>A</li>
+      <li>A</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'CityAlphabet'
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import "../../../assets/styles/variable.styl"
+  .wrapper
+    position absolute
+    top 1.66rem
+    right 0
+    bottom 0
+    .alphabet
+      height 100%
+      display flex
+      flex-direction column
+      justify-content center
+      align-items center
+      li
+        width .4rem
+        line-height .4rem
+        color $bgColor
+        text-align center
+        font-size .26rem
+</style>
